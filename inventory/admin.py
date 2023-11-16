d9044eabb69d3f6bbe9869bb75e29191cf2b7e41
vipin <vipinkumarlamba55@gmail.com>
2023-11-16 12:40:43 +0530

    django admin configuration

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {'slug':('name',)} #we dont have to give the slug field manually
+    # As we enter the title it will automatically generate the slug for it
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title","author", "slug", "price", "in_stock", "created", "updated"]
+    list_filter = ["in_stock", "is_active"]
+    list_editable = ["price", "in_stock"]
+    prepopulated_fields = {"slug": ("title",)}
