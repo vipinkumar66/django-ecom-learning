24520e82aeea89936af0bbc35b121f05025de886
vipin <vipinkumarlamba55@gmail.com>
2023-11-29 16:41:21 +0530

    adding get_categories func as a context in setting

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Product, Category
 
+def get_categories(request):
+    return {"categories": Category.objects.all()}
 
 def all_products(request):
     products = Product.objects.all()
--- orm/settings.py
+++ orm/settings.py
@@ -49,11 +49,13 @@ TEMPLATES = [
         'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
+            # They basically add the context data to the each template that is rendered
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'inventory.views.get_categories',
             ],
         },
     },
