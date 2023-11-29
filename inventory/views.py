ced9aaa023343b10c579d937c2150fa556eb8429
vipin <vipinkumarlamba55@gmail.com>
2023-11-29 10:21:05 +0530

    url, views and template configured

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, "inventory/home.html", {"products":products})
--- orm/urls.py
+++ orm/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("inventory.urls", namespace="store"))
 ]
 if settings.DEBUG == True:
     # This is only used for the development environment and not the production one
