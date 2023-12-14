e7b80178bf646fa4988d5acade3778166e96ca75
vipin <vipinkumarlamba55@gmail.com>
2023-12-14 23:13:22 +0530

    updated the basket code to add the qty and price to the session

--- basket/basket.py
+++ basket/basket.py
@@ -7,5 +7,13 @@ class Basket:
         self.session = request.session
         basket = self.session.get("session_key")
         if "session_key" not in request.session:
-            basket = self.session["session_key"] = {"number":12345}
+            basket = self.session["session_key"] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket['product_id'] = {"price": str(product.price),
+                                         "qty":qty}
+
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .basket import Basket
+from inventory.models import Product
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 # Create your views here.
 def basket_summary(request):
     return render (request, "inventory/basket/summary.html")
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, qty=product_qty)
+
+        return JsonResponse({"qty":product_qty})
